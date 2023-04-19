0a472362d38dde6d8227725fe23a49e101ea2531
Gamzat <gamzik44@gmail.com>
2023-04-19 17:26:44 +0300

    Initial Commit

---
+++ 2UIViews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2UIViews
+//
+//  Created by Гамзат Гасанов on 19.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
